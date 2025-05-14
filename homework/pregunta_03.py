13cd834a72d2e5a4656f32847abf773477dc3b96
Miramirez22 <miramirez@unal.edu.co>
2025-05-13 20:37:47 -0500

    Update pregunta_03.py

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,21 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    with open('files/input/data.csv', 'r') as file:
+        lines = file.readlines()
+
+    sums = {}
+
+    for line in lines:
+        parts = line.strip().split()
+        letter = parts[0]
+        value = int(parts[1])
+
+        if letter in sums:
+            sums[letter] += value
+        else:
+            sums[letter] = value
+
+    result = sorted(sums.items())
+
+    return result
